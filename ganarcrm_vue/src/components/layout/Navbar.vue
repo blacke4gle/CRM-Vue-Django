3308dfc16eaec7a120b38e9a43b49d97b84825c9
blacke4gle <ahmetdemirr0@gmail.com>
2023-05-03 12:27:02 +0300

    first commit

---
+++ ganarcrm_vue/src/components/layout/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong>Ganar CRM</strong>
+            </router-link>
+        </div>
+
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <router-link to="/dashboard/leads" class="navbar-item">Leads</router-link>
+
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <template v-if="!$store.state.isAuthenticated">
+                            <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
+                            <router-link to="/log-in" class="button is-light">Log in</router-link>
+                        </template>
+                        <template v-else>
+                            <router-link to="/dashboard/my-account" class="button is-info">My Account</router-link>
+                        </template>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
---
+++ ganarcrm_vue/src/router/index.js
@@ -0,0 +1,115 @@
+import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
+
+import Home from "../views/Home.vue";
+
+import SignUp from "../views/SignUp.vue";
+import LogIn from "../views/LogIn.vue";
+import Dashboard from "../views/dashboard/Dashboard.vue";
+import MyAccount from "../views/dashboard/MyAccount.vue";
+import Leads from "../views/dashboard/Leads.vue";
+import Lead from "../views/dashboard/Lead.vue";
+import AddLead from "../views/dashboard/AddLead.vue";
+import EditLead from "../views/dashboard/EditLead.vue";
+import AddTeam from "../views/dashboard/AddTeam.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/sign-up",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/log-in",
+    name: "LogIn",
+    component: LogIn,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
+    path: "/dashboard/my-account",
+    name: "MyAccount",
+    component: MyAccount,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
+    path: "/dashboard/add-team",
+    name: "AddTeam",
+    component: AddTeam,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
+    path: "/dashboard/leads",
+    name: "Leads",
+    component: Leads,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
+    path: "/dashboard/leads/add",
+    name: "AddLead",
+    component: AddLead,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
+    path: "/dashboard/leads/:id",
+    name: "Lead",
+    component: Lead,
+    meta: {
+      requireLogin: true,
+    },
+  },
+  {
+    path: "/dashboard/leads/:id/edit",
+    name: "EditLead",
+    component: EditLead,
+    meta: {
+      requireLogin: true,
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (
+    to.matched.some(record => record.meta.requireLogin) &&
+    !store.state.isAuthenticated
+  ) {
+    next("/log-in");
+  } else {
+    next();
+  }
+});
+
+export default router;
